0715b62f8d2cc05e8653c243e459df9bf7ce937e
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-11-05 10:24:58 +1030

    added an example read me for creating a skeleton

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ inquirer
     //pass questions here
     {
         type: 'input',
-        name: 'user-name',
         message: 'What is your name?',
+        name: 'username'
     },
     {  
+        type: 'input',
+        message: 'Please decribe your application',
+        name: 'description'
     }
  ])
+
+//readme skeleton
